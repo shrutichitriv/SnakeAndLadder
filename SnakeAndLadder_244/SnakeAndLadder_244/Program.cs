dab0eb828f594404296f10f8958f1c98fb80aa7e
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-04 23:03:29 +0530

    [Shruti] Perform Snake and Ladder program

---
+++ SnakeAndLadder_244/SnakeAndLadder_244/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadder_244
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+              SL1_SinglePlayer.StartPosition();
+              Console.ReadLine();
+        }
+    }
+}
---
+++ SnakeAndLadder_244/SnakeAndLadder_244/SL1_SinglePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder_244
+{
+    public class SL1_SinglePlayer
+    {
+        public static void StartPosition()
+        {
+            Random rr = new Random();
+            int dice = rr.Next(0, 2); //Genertae A Random Number 0-2
+            //Check If Condition With Random Number
+            if (dice == 0)
+            {
+                Console.WriteLine("You Are Ready To Play=" + dice);
+            }
+            else
+            {
+                Console.WriteLine("You Are Not Ready To Play=" + dice);
+            }
+
+        }
+    }
+}
